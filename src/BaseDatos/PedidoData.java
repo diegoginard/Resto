c8fec265e10275e5c47a91104562147f7c44b0f2
Nandy090587 <141645920+Nandy090587@users.noreply.github.com>
2023-10-09 12:51:08 -0300

    1

--- src/BaseDatos/PedidoData.java
+++ src/BaseDatos/PedidoData.java
@@ -1,7 +1,11 @@
 
 package BaseDatos;
 
+import Entidades.Pedido;
+import Entidades.Producto;
 import java.sql.*;
+import javax.swing.JOptionPane;
+import java.util.Date;
 
 public class PedidoData {
     
@@ -12,7 +16,39 @@ public class PedidoData {
         con = Conexion.getConexion();
     }
     
-    
+    public void guardarPedido(Pedido ped){
+        
+        String sql="INSERT INTO pedido(idMesa,nombreMesero,fechaHora, importe, cobrada) VALUES (?,?,?,?.?)"; 
+      
+        try {
+            
+            PreparedStatement ps = con.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            
+            ps.setInt(1, ped.getMesa().getIdMesa());
+            ps.setString(2, ped.getNombreMesero());
+            ps.setTimestamp(3, new java.sql.Timestamp(ped.getFechaHora().getTime()));
+            ps.setDouble(4, ped.getImporte());
+            ps.setBoolean(5, ped.isCobrada());
+            
+            ps.executeUpdate();
+            ResultSet rs = ps.getGeneratedKeys();
+           
+            if (rs.next()) {
+
+               ped.setIdPedido(rs.getInt(1));
+                
+               JOptionPane.showMessageDialog(null, "Pedido Guardado ");
+            
+            }
+            
+            ps.close();
+            
+        } catch (SQLException ex) {
+            
+            JOptionPane.showMessageDialog(null, "Error al acceder a la tabla " + ex.getMessage());
+            
+        }
+    }
     
     
     
--- src/resto/Resto.java
+++ src/resto/Resto.java
@@ -2,28 +2,34 @@
 package resto;
 
 import BaseDatos.MesaData;
+import BaseDatos.PedidoData;
 import BaseDatos.ProductoData;
 import Entidades.Mesa;
+import Entidades.Pedido;
 import Entidades.Producto;
+import java.sql.Date;
 
 
 public class Resto {
 
     public static void main(String[] args) {
-       ProductoData pd = new ProductoData();
+//       ProductoData pd = new ProductoData();
        
 //       Producto pro = new Producto("tacos" , 2800.000 ,5, true);
-       System.out.println(pd.listarProductos()); //Funciona
+//       System.out.println(pd.listarProductos()); //Funciona
 //       pd.guardarProducto(pro);  //Funciona
 //       pd.ModificarProducto(pro); //Funciona
 //       pd.eliminarProducto(5);  //Funciona
-       MesaData md = new MesaData();
-        System.out.println(md.listarMesas());
+//       MesaData md = new MesaData();
+//       System.out.println(md.listarMesas());
 //       Mesa mesa= new Mesa(1,"",4);
 //       md.crearMesa(mesa);  //Funciona
 //       md.eliminarMesa(4);  //Funciona
 //       Mesa mesa= new Mesa(1,1,"Ocupado",6);
 //       md.modificarMesa(mesa);  //Funciona
+PedidoData pd = new PedidoData();
+Pedido pedido = new Pedido(1,"Carlos",new Date(),5000.00,true);
+pd.guardarPedido(pedido);
 
         }
 
