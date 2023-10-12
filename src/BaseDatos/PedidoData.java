79ad0fb8f2f2a9438791bb7b5133ba9d7d466f22
diegoginard <diegoginard@hotmail.com>
2023-10-12 12:30:35 -0300

    utyu

--- src/BaseDatos/PedidoData.java
+++ src/BaseDatos/PedidoData.java
@@ -1,6 +1,7 @@
 
 package BaseDatos;
 
+import Entidades.Mesa;
 import Entidades.Pedido;
 import java.sql.*;
 import javax.swing.JOptionPane;
@@ -14,14 +15,17 @@ public class PedidoData {
         con = Conexion.getConexion();
     }
     
-    public void guardarPedido(Pedido ped){
+    Pedido pe = new Pedido();
+    Mesa me =new Mesa();
         
-        String sql = "INSERT INTO pedido (idMesa , nombreMesero , fechaHora , importe , cobrada , estado) VALUES (?,?,?,?,?)"; 
-        
+    public void guardarPedido(Pedido ped) {
+
+        String sql = "INSERT INTO pedido (idMesa , nombreMesero , fechaHora , importe , cobrada , estado) VALUES (?,?,?,?,?,?)";
+
         try {
-            
+
             PreparedStatement ps = con.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
-            
+
             ps.setInt(1, ped.getMesa().getIdMesa());
             ps.setString(2, ped.getNombreMesero());
             ps.setTimestamp(3, Timestamp.valueOf(ped.getFechaHora()));
@@ -30,38 +34,70 @@ public class PedidoData {
             ps.setString(6, ped.getEstado());
             ps.executeUpdate();
             ResultSet rs = ps.getGeneratedKeys();
-           
+
             if (rs.next()) {
 
-               ped.setIdPedido(rs.getInt(1));
-                
-               JOptionPane.showMessageDialog(null, "Pedido Guardado ");
-            
+                ped.setIdPedido(rs.getInt(1));
+
+                JOptionPane.showMessageDialog(null, "Pedido Guardado ");
+
             }
-            
+
             ps.close();
-            
-        }catch (SQLException ex) {
-            
+
+        } catch (SQLException ex) {
+
             JOptionPane.showMessageDialog(null, "Error al acceder a la tabla " + ex.getMessage());
-            
+
         }
     }
-    
-      public void eliminarPedido (int idPedido){
-        
+
+    public void eliminarPedido(int idPedido) {
+
         String sql = "DELETE FROM pedido WHERE idPedido = ?";
-        
+
         try {
             PreparedStatement ps = con.prepareStatement(sql);
             ps.setInt(1, idPedido);
             ps.executeUpdate();
             JOptionPane.showMessageDialog(null, "Pedido eliminado ");
             ps.close();
+
+        } catch (SQLException ex) {
+
+            JOptionPane.showMessageDialog(null, "No se pudo acceder a la tabla pedidos " + ex.getMessage());
+        }
+    }
+    
+    public void modificarPedido(int id){
+        
+        String sql = "UPDATE pedido SET idPedido = ? , idMesa = ? , nombreMesero = ? , "
+                + "fechaHora = ? , importe = ? , cobrada = ? , estado = ? WHERE idPedido = ?";
+        
+        try {
+            
+            PreparedStatement ps= con.prepareStatement(sql);
+
+            ps.setInt(1, pe.getIdPedido());
+            ps.setInt(2, me.getIdMesa());
+            ps.setString(3, pe.getNombreMesero());
+            ps.setTimestamp(4,Timestamp.valueOf(pe.getFechaHora()));
+            ps.setDouble(5, pe.getImporte());
+            ps.setBoolean(6, pe.isCobrada());
+            ps.setString(7, pe.getEstado());
+            ps.setInt(8,id);
+            int exito= ps.executeUpdate();
+            
+            if (exito==1) {
+                
+                JOptionPane.showMessageDialog(null, "Pedido Modificado");
+           
+            }
             
         } catch (SQLException ex) {
             
-            JOptionPane.showMessageDialog(null, "No se pudo acceder a la tabla pedidos "+ex.getMessage());
+            JOptionPane.showMessageDialog(null,"Error al acceder a la tabla Pedido"+ex.getMessage());
+        
         }
     }
 }
--- src/resto/Resto.java
+++ src/resto/Resto.java
@@ -27,7 +27,9 @@ public class Resto {
 //       md.eliminarMesa(4);  //Funciona
        Mesa mesa = new Mesa(2, 1, "Ocupado", 6);
 //       md.modificarMesa(mesa);  //Funciona
-       Pedido p1 = new Pedido(mesa, "Carlos", LocalDateTime.of(2023, 10, 10, 11, 00), 5000, true);
+       Pedido p1 = new Pedido(mesa, "Carlos"
+               , LocalDateTime.of(2023, 10, 10, 11, 00)
+               , 5000, true, "entregado");
        PedidoData pd = new PedidoData();
        pd.guardarPedido(p1); //Funciona
 //        pd.eliminarPedido(4);
