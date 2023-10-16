95e735935a420a8615220422feac8f0f6cf4ae13
diegoginard <diegoginard@hotmail.com>
2023-10-16 18:36:55 -0300

    lkjlk

--- src/BaseDatos/MesaData.java
+++ src/BaseDatos/MesaData.java
@@ -2,14 +2,9 @@
 package BaseDatos;
 
 import Entidades.Mesa;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.ArrayList;
-import java.util.List;
-import javax.swing.JOptionPane;
+import java.sql.*;
+import java.util.*;
+import javax.swing.*;
 
 
 public class MesaData {
--- src/BaseDatos/ProductoData.java
+++ src/BaseDatos/ProductoData.java
@@ -172,5 +172,4 @@ public class ProductoData {
         return productos;
     
     }
-    
 }
